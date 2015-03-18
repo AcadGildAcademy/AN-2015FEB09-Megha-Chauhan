3453a2dca0258f0fc50b05ca34a0917ee7f783c2
MeghaChauhan16 <meghachauhan16.yahoo.com>
2015-03-18 12:52:19 +0530

    on click of signin button not showing msg

--- Main_project/app/src/main/java/com/example/megha/main_project/ServiceHandler.java
+++ Main_project/app/src/main/java/com/example/megha/main_project/ServiceHandler.java
@@ -2,6 +2,8 @@ package com.example.megha.main_project;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
@@ -9,12 +11,66 @@ import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by megha on 3/14/2015.
  */
-public class ServiceHandler
+public class ServiceHandler {
+
+    static String response = null;
+    public final static int GET = 1;
+    public final static int POST = 2;
+
+    public String makeServiceCall(String url, int method) {
+        return this.makeServiceCall(url, method, null);
+    }
+
+    public String makeServiceCall(String url, int method,
+                                  List<NameValuePair> params) {
+        try {
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            HttpEntity httpEntity = null;
+            HttpResponse httpResponse = null;
+
+            if (method == POST) {
+                HttpPost httpPost = new HttpPost(url);
+                // adding post params
+                if (params != null) {
+                    httpPost.setEntity(new UrlEncodedFormEntity(params));
+                }
+
+                httpResponse = httpClient.execute(httpPost);
+
+            } else if (method == GET) {
+                // appending params to url
+                if (params != null) {
+                    String paramString = URLEncodedUtils
+                            .format(params, "utf-8");
+                    url += "?" + paramString;// id=123
+                }
+                HttpGet httpGet = new HttpGet(url);
+
+                httpResponse = httpClient.execute(httpGet);
+
+            }
+            httpEntity = httpResponse.getEntity();//key ,value
+            response = EntityUtils.toString(httpEntity);
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
+}
+/*public class ServiceHandler
 {
     static String response=null;
     public final static int GET=1;
@@ -27,7 +83,6 @@ public class ServiceHandler
 
     public String makeServiceCall(String url,int method,List params)
     {
-
         try
         {
             DefaultHttpClient httpClient=new DefaultHttpClient();
@@ -46,8 +101,8 @@ public class ServiceHandler
             {
                 if(params!=null)
                 {
-                    String paramString = URLEncodedUtils.format(params, "utf-8");
-                    url += "?" + paramString;
+                    String paramString = URLEncodedUtils.format(params,"utf-8");
+                    url += "?"+ paramString;
                 }
                 HttpGet httpGet=new HttpGet(url);
                 httpResponse=httpClient.execute(httpGet);
@@ -59,4 +114,4 @@ public class ServiceHandler
         }
         return response;
     }
-}
+}*/
--- Main_project/app/src/main/java/com/example/megha/main_project/Sign_in_mainclass.java
+++ Main_project/app/src/main/java/com/example/megha/main_project/Sign_in_mainclass.java
@@ -2,26 +2,36 @@ package com.example.megha.main_project;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.security.Key;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by megha on 3/11/2015.
  */
+
 public class Sign_in_mainclass extends Activity {
     EditText user_name, user_password;
     Button signin, cancel;
@@ -30,22 +40,21 @@ public class Sign_in_mainclass extends Activity {
 
     final String url = "http://bishasha.com/json/whdeal_login.php";
     String TAG_SUCCESS = "success";
-    String TAG_FAIL = "fail";
-    ServiceHandler serviceHandler = new ServiceHandler();
+    JSONArray jsonArray = null;
     private ProgressDialog pDialog;
     ArrayList<HashMap<String, String>> dataList;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        Toast.makeText(getApplicationContext(),"entered",Toast.LENGTH_LONG).show();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signin_page);
-
-
-
         dataList = new ArrayList<HashMap<String, String>>();
         user_name = (EditText) findViewById(R.id.et_username);
         user_password = (EditText) findViewById(R.id.et_password);
+        remember_password=(CheckBox)findViewById(R.id.remember_check);
+        loadMySavedPrefrences();
         signin = (Button) findViewById(R.id.signin_btn);
         cancel = (Button) findViewById(R.id.cancel_btn);
         signin.setOnClickListener(new View.OnClickListener() {
@@ -55,10 +64,44 @@ public class Sign_in_mainclass extends Activity {
                 String username=user_name.getText().toString();
                 String password=user_password.getText().toString();
                 new SubClass().execute();
+
+                saveMySharedPrefrences("CHECKBOX",remember_password.isChecked());
+                String str1=user_name.getText().toString();
+                String str2=user_password.getText().toString();
+                saveMySharedPrefrences("USERNAME",str1);
+                saveMySharedPrefrences("PASSWORD",str2);
             }
         });
     }
-    private class SubClass extends AsyncTask<Void,Void,Void>
+
+    private void saveMySharedPrefrences(String Key, boolean values)
+    {
+        SharedPreferences sp =PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor ed =sp.edit();
+        ed.putBoolean(Key,values);
+        ed.commit();
+    }
+
+    private void saveMySharedPrefrences(String Key, String values)
+    {
+        SharedPreferences sp=PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor ed =sp.edit();
+        ed.putString(Key,values);
+        ed.commit();
+    }
+
+    private void loadMySavedPrefrences()
+    {
+        SharedPreferences sp=PreferenceManager.getDefaultSharedPreferences(this);
+        String str1=sp.getString("USERNAME","abc@gmail.com");
+        String str2=sp.getString("PASSWORD","def");
+        Boolean ck=sp.getBoolean("CHECKBOX",true);
+        user_name.setText(str1);
+        user_password.setText(str2);
+        remember_password.setChecked(ck);
+    }
+
+    private class SubClass extends AsyncTask<Void,Integer,Void>
     {
         boolean failure = false;
         @Override protected void onPreExecute()
@@ -66,46 +109,59 @@ public class Sign_in_mainclass extends Activity {
             super.onPreExecute();
             pDialog = new ProgressDialog(Sign_in_mainclass.this);
             pDialog.setMessage("Attempting for login...");
-            pDialog.setIndeterminate(false);
-            pDialog.setCancelable(true);
-            pDialog.show(); }
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
 
-    @Override
-    protected Void doInBackground(Void... params)
-    {
-        int success;
-
-        try
+        @Override
+        protected Void doInBackground(Void... params)
         {
-                List<NameValuePair> param = new ArrayList<NameValuePair>();
-                String username=user_name.getText().toString();
-                String password=user_password.getText().toString();
+            int success;
+            String username=user_name.getText().toString();
+            String password=user_password.getText().toString();
+            try
+            {
+                Toast.makeText(getApplicationContext(),"helloo",Toast.LENGTH_LONG).show();
+                List param = new ArrayList();
                 param.add(new BasicNameValuePair("email", username));
                 param.add(new BasicNameValuePair("password", password));
                 Log.d("request!", "starting");
+                ServiceHandler serviceHandler = new ServiceHandler();
                 String jsonString=serviceHandler.makeServiceCall(url,1,param);
                 JSONObject jsonObject=new JSONObject(jsonString);
                 success=jsonObject.getInt(TAG_SUCCESS);
-                if(success==1)
-                {
-                    Log.d("login!","success");
-                }
-                else
-                {
-                    Log.d("login!", "fail");
-                }
-        }
-        catch(Exception e)
-        {
+                Log.d("login!","success");
+            }
+            catch(Exception e)
+            {
                 e.getStackTrace();
+            }
+            return null;
         }
-        return null;
-    }
-    @Override
-    protected void onPostExecute(Void s)
-    {
+
+        protected void onPostExecute(Void s)
+        {
             super.onPostExecute(s);
             pDialog.dismiss();
-    }
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values)
+        {
+            super.onProgressUpdate(values);
+            int success = values[0];
+            if (success == 1)
+            {
+                Log.d("Login Successful!", "Login Success");
+                Toast.makeText(getApplicationContext(),"login success",Toast.LENGTH_LONG).show();
+                Intent i = new Intent(Sign_in_mainclass.this, First_page.class);
+                finish();
+            }
+            else
+            {
+                Log.d("Login Failure!","login fail");
+                Toast.makeText(Sign_in_mainclass.this,"login fail",Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
