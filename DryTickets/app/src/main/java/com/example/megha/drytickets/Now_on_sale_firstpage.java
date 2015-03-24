e669b868256d8164148651439ec18a0ff6a68111
MeghaChauhan16 <meghachauhan16.yahoo.com>
2015-03-24 12:52:10 +0530

    please check now_on_sale_page code

--- DryTickets/app/src/main/java/com/example/megha/drytickets/Now_on_sale_firstpage.java
+++ DryTickets/app/src/main/java/com/example/megha/drytickets/Now_on_sale_firstpage.java
@@ -1,7 +1,97 @@
 package com.example.megha.drytickets;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.util.ArrayList;
+import java.util.List;
 /**
  * Created by megha on 3/20/2015.
  */
-public class Now_on_sale_firstpage {
+public class Now_on_sale_firstpage extends Activity
+{
+    List<Events> eventList;
+    EventAdapter arrayAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.now_on_sale_page);
+        eventList = new ArrayList<Events>();
+        new JSONAsyncTask().execute("http://microblogging.wingnity.com/JSONParsingTutorial/jsonActors");
+
+        ListView listview = (ListView)findViewById(R.id.custom_list);
+        arrayAdapter = new EventAdapter(getApplicationContext(), R.layout.now_on_sale_page, (ArrayList<Events>) eventList);
+        listview.setAdapter(arrayAdapter);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), eventList.get(position).getName(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    class JSONAsyncTask extends AsyncTask<String, Void, Boolean> {
+        ProgressDialog dialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog = new ProgressDialog(Now_on_sale_firstpage.this);
+            dialog.setMessage("Loading, please wait");
+            dialog.setTitle("Connecting server");
+            dialog.show();
+            dialog.setCancelable(false);
+        }
+
+        @Override
+        protected Boolean doInBackground(String... urls) {
+            try {
+
+                //------------------>>
+                HttpGet httppost = new HttpGet(urls[0]);
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpResponse response = httpclient.execute(httppost);
+                int status = response.getStatusLine().getStatusCode();
+                if (status == 200) {
+                    HttpEntity entity = response.getEntity();
+                    String data = EntityUtils.toString(entity);
+                    JSONObject jsonObject = new JSONObject(data);
+                    JSONArray jsonArray = jsonObject.getJSONArray("actors");
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject object = jsonArray.getJSONObject(i);
+                        Events events = new Events();
+                        events.setImage(object.getString("image"));
+                        eventList.add(events);
+                    }
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+            return false;
+        }
+
+        protected void onPostExecute(Boolean result) {
+            dialog.cancel();
+            arrayAdapter.notifyDataSetChanged();
+            if(result == false)
+                Toast.makeText(getApplicationContext(), "Unable to fetch data from server", Toast.LENGTH_LONG).show();
+
+        }
+    }
 }
